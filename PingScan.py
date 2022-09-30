e372bff39cc577af3980ed3858909faf12656093
shreyash dhanawade <68527969+shreyash00007@users.noreply.github.com>
2022-09-30 22:05:29 +0530

    Update PingScan.py

--- PingScan.py
+++ PingScan.py
@@ -1,13 +1,14 @@
+# Installing pyping 
 pip install pyping  
 
+# Imports
 import pyping
 import csv
 
-
 r = pyping.ping('google.com')
 
-if r.ret_code == 0:
-    print("Success")
+if r.ret_code == 0: #if condition
+    print("Success") 
 else:
     print("Failed with {}".format(r.ret_code))
 print("Congratulaions Ping Successful")
